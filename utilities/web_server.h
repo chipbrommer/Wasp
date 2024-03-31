fcfe39b593c187ad8bbad09f0f27b571b4edebc7
Chip Brommer <chipbrommer@gmail.com>
2024-03-31 17:11:56 -0500

    add more to webserver

--- utilities/web_server.h
+++ utilities/web_server.h
@@ -14,6 +14,7 @@
 //
 #include "../external/civetweb/CivetServer.h"   // Civet header
 #include "log_client.h"             // Log Client
+#include "constants.h"				// Buffer size
 //
 /////////////////////////////////////////////////////////////////////////////////
 
@@ -27,23 +28,54 @@ public:
     
     ~WebServer() { Stop(); }
 
+    /// @brief Sets the port for 
+    /// @param port 
     void SetPort(int port);
 
+    /// @brief Sets the target directory for web requests
+    /// @param path 
     void SetDirectory(std::string path);
 
+	/// @brief Starts the web server process. NOTE: This blocks the calling thread
     void Start();
 
+    /// @brief Stops the web server process.
     void Stop();
+
+	/// @brief handles config page request
+	/// @param c - the connection for the page request
+	/// @param hm - the page request data
+	void HandleConfigPage(mg_connection* c);
+
+	/// @brief handles the layout page request
+	/// @param c - the connection for the page request
+	/// @param bodyContent - the content to be embedded in the layout page
+	void HandleLayoutPage(mg_connection* c, std::string bodyContent);
+
+	/// @brief handles the reboot action
+	/// @param c - the connection for the page request
+	void HandleReboot(mg_connection* c);
+
+	std::mutex m_connectionLock;
+	std::map<struct mg_connection*, bool> m_connections;
+	std::string m_rootDir;			// the root directory that will be accessed for web files
 
 protected:
 
 private:
+	/// @brief Attempts to parse a specified variable from the specified content
+	/// @param name - [in] - the variable to parse
+	/// @param data - [in] - the data to parse the variable from
+	/// @return - the string format of the variable value
+	std::string Parse(const std::string name, const char* data);
 
-    std::string     m_name;
+    std::string     m_name					= "";
     CivetServer*    m_server;
     LogClient&      m_logger;
-    int             m_port;
-    std::string     m_directory;
-    bool            m_running;
-    const mg_context* m_context;
+    int             m_port					= 0;
+    std::string     m_directory				= "";
+    bool            m_running				= false;
+	const mg_context* m_context				= {};
+	std::string		m_listeningAddress		= "";
+	char			m_buffer[BUFFER_SIZE]	= {};
 };
